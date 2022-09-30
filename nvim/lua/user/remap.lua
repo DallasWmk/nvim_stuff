4740af7959535d54843cad854d0bde5af4b91ba8
DallasWmk <69164447+DallasWmk@users.noreply.github.com>
2022-09-30 12:59:04 -0700

    Update remap.lua

--- nvim/lua/user/remap.lua
+++ nvim/lua/user/remap.lua
@@ -1,7 +1,8 @@
 local nnoremap = require("user.keymap").nnoremap
 local inoremap = require("user.keymap").inoremap
+local vnoremap = require("user.keymap").vnoremap
 -- get into the menu better
-nnoremap("<leader>e", "<cmd>Ex<CR>")
+nnoremap("<leader>e", "<cmd>wincmd v <bar> :Ex <bar> :vertical resize 20<CR>")
 
 -- better window navigation
 nnoremap("<C-h>", "<C-w>h")
@@ -22,8 +23,18 @@ inoremap('{', '{}<Left>')
 inoremap('"', '""<Left>')
 inoremap('"', '""<Left>')
 
+-- Esc remapping
+inoremap('jj', '<Esc>')
+vnoremap('jj', '<Esc>')
+
 --lsp remaps
-nnoremap("<leader>gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+nnoremap("<silent>gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
 nnoremap("<leader>gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
 nnoremap("<leader>gr", "<cmd>lua vim.lsp.buf.references()<CR>")
 nnoremap("<leader>gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+
+-- telescope remaps
+nnoremap("<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<CR>")
+nnoremap("<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<CR>")
+nnoremap("<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<CR>")
+nnoremap("<leader>fh", "<cmd>lua require('telescope.builtin').()help_tags<CR>")
