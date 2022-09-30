ab39085a15cb3ed1c50b6728276b998477d5b751
DallasWmk <69164447+DallasWmk@users.noreply.github.com>
2022-09-30 12:57:41 -0700

    Update packer.lua

--- nvim/lua/user/packer.lua
+++ nvim/lua/user/packer.lua
@@ -4,18 +4,28 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-		-- Packer can manage itself
+                -- Packer can manage itself
         use 'ThePrimeagen/vim-be-good'
-		use 'wbthomason/packer.nvim'
-		use 'folke/tokyonight.nvim'
-		use 'gruvbox-community/gruvbox'
-		use 'neovim/nvim-lspconfig'
-		use 'williamboman/nvim-lsp-installer'
-		use 'jose-elias-alvarez/null-ls.nvim'
+                use 'wbthomason/packer.nvim'
+                use 'folke/tokyonight.nvim'
+                use 'gruvbox-community/gruvbox'
+                use 'neovim/nvim-lspconfig'
+                use 'williamboman/nvim-lsp-installer'
+                use 'jose-elias-alvarez/null-ls.nvim'
         use 'nvim-lua/plenary.nvim'
-        use 'hrsh7th/nvim-compe'
+        use 'hrsh7th/nvim-cmp'
+        use 'hrsh7th/cmp-buffer'
+        use 'hrsh7th/cmp-path'
+        use 'hrsh7th/cmp-cmdline'
+        use 'hrsh7th/cmp-nvim-lsp'
+        use 'L3MON4D3/LuaSnip'
+        use 'rafamadriz/friendly-snippets'
         use {
             'nvim-treesitter/nvim-treesitter',
             run = ":TSUpdate",
+            }
+        use {
+            'nvim-telescope/telescope.nvim', tag = '0.1.0',
+            requires = { {'nvim-lua/plenary.nvim'} }
             }
 end)
